fdf52828fed9e7e517993129e708c154aacddecb
Velizar Hristov <velizar.hs@gmail.com>
2022-12-18 20:32:24 +0100

    Fetch all plants and list them on the homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,41 @@
+import { useState, useEffect } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
+import { Plant } from './plant.entity'
 
-function App() {
+export default function App() {
+  const [allPlants, setAllPlants] = useState<Plant[]>([]);
+
+  useEffect(() => {
+    const f = async (): Promise<void> => {
+      const res = await fetch("http://127.0.0.1:5000/plants");
+      const json = await res.json();
+      setAllPlants(json);
+    };
+    f();
+  }, []);
+  const plants = allPlants.map(plant =>
+    <tr>
+      <td>{plant.id}</td>
+      <td>{plant.name}</td>
+    </tr>
+  );
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          Listing plants
+        </h1>
+        <table>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+          </tr>
+          {plants}
+        </table>
       </header>
     </div>
   );
 }
-
-export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -6,8 +6,9 @@ import App from './App';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
